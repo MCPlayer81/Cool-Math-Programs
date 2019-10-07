4408f22483411773f925788fc28d2b1d1424938d
Freddy Baker <romecivi34@gmail.com>
2019-10-07 19:49:35 -0400

    Updated the formating on some stuff

---
+++ Polynomial Factorer/classes.py
@@ -0,0 +1,11 @@
+class equation:
+    """Equation Class"""
+    expression = ""
+    degree = 0
+    terms = []
+    coefficients = []
+    possible_roots = []
+    roots = []
+    formatted_roots = []
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cool-Math-Programs
 A place where I put all my cool program that do math things :)
+
+Currently consists of a Polynomial Factorer that half works :p
